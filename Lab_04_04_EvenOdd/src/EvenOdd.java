2e77014853f4f79f0d0e4c7256a75f2230f3cade
Hunter Jackson <jacksohb@mail.uc.edu>
2024-02-04 13:24:46 -0500

    ExtraCredit

---
+++ Lab_04_04_EvenOdd/src/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 8;
+        if (num % 2 == 0) {
+            System.out.println("Entered number is even!");
+        }
+        else {
+                System.out.println("Entered number is odd!");
+            }
+    }
+}
