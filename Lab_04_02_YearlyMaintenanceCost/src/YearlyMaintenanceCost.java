8a2cf52b15aa9bc3cc01eb7cc2c68a208280106d
Hunter Jackson <jacksohb@mail.uc.edu>
2024-02-04 21:02:15 -0500

    Fixed Variable not matching

--- Lab_04_02_YearlyMaintenanceCost/src/YearlyMaintenanceCost.java
+++ Lab_04_02_YearlyMaintenanceCost/src/YearlyMaintenanceCost.java
@@ -5,7 +5,7 @@ public class YearlyMaintenanceCost {
         int fallCost = 400;
         int winterCost = 750;
         int yearlyCost = 0;
-        int yearlyCost = springCost + summerCost + fallCost + winterCost;
+        yearlyCost = springCost + summerCost + fallCost + winterCost;
         System.out.println("Your seasonal maintenance costs (spring: " + springCost + ", summer: " + summerCost + ", fall: " + fallCost + ", and winter: " + winterCost + ") add up to a total yearly cost of " + yearlyCost + ".");
     }
 }
