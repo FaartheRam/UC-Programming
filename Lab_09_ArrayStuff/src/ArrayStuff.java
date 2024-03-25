71636e9d9fcfa503ad117709ea9b8b00a7d6498a
Hunter Jackson <jacksohb@mail.uc.edu>
2024-03-24 21:08:21 -0400

    Final Push

--- Lab_09_ArrayStuff/src/ArrayStuff.java
+++ Lab_09_ArrayStuff/src/ArrayStuff.java
@@ -11,19 +11,19 @@ public class ArrayStuff {
         //Part 1: DataPoints Array
 
         //a
-        int[] dataPoints = new int[100]; // set datapoints variable (100)
+        int[] dataPoints = new int[100]; // set datapoints (100)
 
         //b
-        for (int i = 0; i < dataPoints.length; i++) {
+        for (int i = 0; i < dataPoints.length; i++) { // start at the beginning, as long as it is less than the array, keep looping - increment by 1
             dataPoints[i] = rnd.nextInt(100) + 1; // random number between 1 and 100
         }
 
         //c
-        for (int j = 0; j < dataPoints.length; j++) {
-            if (j < dataPoints.length - 1) {
-                System.out.print(dataPoints[j] + " | "); // line between data points
+        for (int j = 0; j < dataPoints.length; j++) { // start at 0 - ""
+            if (j < dataPoints.length - 1) { // counting down from 100
+                System.out.print(dataPoints[j] + " | "); // display datapoint and line between data points
             } else {
-                System.out.print(dataPoints[j]);
+                System.out.print(dataPoints[j]); // end loop
             }
         }
 
@@ -34,7 +34,7 @@ public class ArrayStuff {
 
         //Part 2: Linear Search
         Scanner in = new Scanner(System.in);
-        int count = 0;
+        int count = 0; // set count variable
 
         //a
         int ans = SafeInput.getRangedInt(in, "Enter an integer value between 1 and 100", 1, 100);
@@ -58,8 +58,8 @@ public class ArrayStuff {
     public static double getAverage(int values[]) {
         double sum = 0; // sum variable
         double average; // average variable
-        for (int i = 0; i < values.length; i++) {
-            sum += values[i];
+        for (int i = 0; i < values.length; i++) { // start at the begging of the for loop, as long as it is less than array-length - keep looping. after each loop, increment i by 1
+            sum += values[i]; // sum = sum + value
         }
         average = sum/values.length;
         System.out.printf("\nThe sum of the datapoints is %.2f and the average is %.2f.\n", sum, average);
@@ -67,60 +67,60 @@ public class ArrayStuff {
     }
 
     public static int min(int values[]) {
-        int min = values[0];
-        for (int n = 0; n < values.length; n++) {
-            if (values[n] < min) {
-                min = values[n];
+        int min = values[0]; // set variable
+        for (int n = 0; n < values.length; n++) { // start at the begging of the for loop, as long as it is less than array-length - keep looping. after each loop, increment n by 1
+            if (values[n] < min) { // if value is less than min
+                min = values[n]; // variable = value
             }
         }
         System.out.printf("\nThe min value is %d.\n", min);
-        return min;
+        return min; // return variable
     }
 
     public static int max(int values[]) {
-        int max = values[0];
-        for (int n = 0; n < values.length; n++) {
+        int max = values[0]; //variable for max value
+        for (int n = 0; n < values.length; n++) { // start at the begging of the for loop, as long as it is less than array-length - keep looping. after each loop, increment n by 1
             if (values[n] > max) {
-                max = values[n];
+                max = values[n]; // set max as the highest value in the array
             }
         }
-        System.out.printf("\nThe max value is %d.\n", max);
-        return max;
+        System.out.printf("\nThe max value is %d.\n", max); // display max value in the array
+        return max; // return variable
     }
 
     public static int occurenceScan(int values[], int target) {
         int count = 0;
-        for (int l = 0; l < values.length; l++) {
-            if (target == values[l]) {
-                count++;
+        for (int l = 0; l < values.length; l++) { // start at the begging of the for loop, as long as it is less than array-length - keep looping. after each loop, increment l by 1
+            if (target == values[l]) { // if input matches value
+                count++; // increment count by 1
             }
         }
-        System.out.printf("\n%d appeared in the DataPoints array %d times.\n", target, count);
-        return count;
+        System.out.printf("\n%d appeared in the DataPoints array %d times.\n", target, count); // how many times input value appears
+        return count; // return variable
     }
 
     public static int sum(int values[]) {
-        int sumOfValues = 0;
-        for (int i = 0; i < values.length; i++) {
-            sumOfValues += values[i];
+        int sumOfValues = 0; // sum variable
+        for (int i = 0; i < values.length; i++) { // start at the begging of the for loop, as long as it is less than array-length - keep looping. after each loop, increment i by 1
+            sumOfValues += values[i]; // sum = sums + values
         }
-        return sumOfValues;
+        return sumOfValues; // return variable
     }
 
     public static boolean contains(int values[], int target) {
         int matches = -1; // set counter to -1
-        for (int m = 0; m < values.length; m++) {
-            if (target == values[m]) {
-                matches = m;
-                break; // break the
+        for (int m = 0; m < values.length; m++) { // start at the begging of the for loop, as long as it is less than array-length - keep looping. after each loop, increment i by 1
+            if (target == values[m]) { // if target matches a value
+                matches = m; // input equals value
+                break; // terminate the loop
             }
         }
-        if (matches != -1) {
-            System.out.printf("\nThe value %d was found at array index %d.\n", target, matches);
-            return true;
-        } else {
-            System.out.printf("\nThe value %d was not found.\n", target);
-            return false;
+        if (matches != -1) { // if matches does not equal -1 = number found
+            System.out.printf("\nThe value %d was found at array index %d.\n", target, matches); // display matched variable and match = 1st place in array
+            return true; // return boolean true
+        } else { // this means matches if false
+            System.out.printf("\nThe value %d was not found.\n", target); // display input not found
+            return false; // return boolean false
         }
     }
 }
